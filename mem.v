9d920b941b2bf9639091b0ae706486e245deb0fd
lauranagamine1 <laura.nagamine@utec.edu.pe>
2025-06-29 21:59:37 -0500

    changing to .mem for better access to hexadecimal code

--- mem.v
+++ mem.v
@@ -32,7 +32,8 @@ module mem (
 	input wire [31:0] wd;
 	output wire [31:0] rd;
 	reg [31:0] RAM [63:0];
-	initial $readmemh("memfile.dat", RAM);
+    initial $readmemh("memfile.mem", RAM);
+
 	assign rd = RAM[a[31:2]]; // word aligned
 	always @(posedge clk)
 		if (we)
