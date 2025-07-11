8217efa4bc6fcc03dd1481535524b3b1110a346d
Chimichami <cesar.carranza.r@utec.edu.pe>
2025-07-11 02:46:12 -0500

    1/3 cambios en source.v

--- source.v
+++ source.v
@@ -1,5 +1,21 @@
 module HexTo7Segment (input[3:0] digit, output reg[7:0] catode);
-    ....
+    casex(input[3:0])
+        4'b0000: catode = 7'b0111111; //0
+        4'b0001: catode = 7'b0000110; //1
+        4'b0010: catode = 7'b1011011; //2
+        4'b0011: catode = 7'b1001111; //3
+        4'b0100: catode = 7'b1100110; //4
+        4'b0101: catode = 7'b1101101; //5
+        4'b0110: catode = 7'b1111101; //6
+        4'b0111: catode = 7'b0000111; //7
+        4'b1000: catode = 7'b1111111; //8
+        4'b1001: catode = 7'b1100111; //9
+        4'b1010: catode = 7'b1110111; //A 
+        4'b1011: catode = 7'b; //B 
+        4'b1100: catode = 7'b; //C 
+        4'b1101: catode = 7'b; //D 
+        4'b1110: catode = 7'b; //E 
+        4'b1111: catode = 7'b; //F
 endmodule
 
 module CLKdivider(input clk, input reset, output reg t);
